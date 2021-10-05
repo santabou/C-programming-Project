07ca24c312acee693491ec0c08d469faaef5744b
crystilonz <64011729@kmitl.ac.th>
2021-10-05 16:50:14 +0700

    Main program

--- function.c
+++ function.c
@@ -1,7 +1,47 @@
 // base functions
+#include <stdio.h>
+#include <stdlib.h>
 
 void view();
 void create();
 void delete();
 void edit();
 void select();
+
+void select()
+{
+    printf("Your selection: ");
+    int selection = getchar();
+    switch(selection){
+        case 1:
+            view();
+            break;
+        case 2:
+            create();
+            break;
+        case 3:
+            delete();
+            break;
+        case 4:
+            edit();
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid Selection!");
+            select();
+    }
+}
+
+void menu()
+{
+    printf("MENU\n");
+    printf("Enter a number to proceed:\n");
+    printf("1. View student data\n");
+    printf("2. Create new student data\n");
+    printf("3. Delete student data\n");
+    printf("4. Edit student data\n");
+    printf("5. Exit\n\n");
+
+    select();
+}
--- main.c
+++ main.c
@@ -19,48 +19,6 @@ typedef struct
     Student* next;
 }Node;
 
-void menu();
-void select();
-
-void select()
-{
-
-    printf("Your selection: ");
-    int selection = getchar();
-    switch(selection){
-        case 1:
-            view();
-            break;
-        case 2:
-            create();
-            break;
-        case 3:
-            delete();
-            break;
-        case 4:
-            edit();
-            break;
-        case 5:
-            exit(0);
-        default:
-            printf("Invalid Selection!");
-            select();
-    }
-}
-
-void menu()
-{
-    printf("MENU\n");
-    printf("Enter a number to proceed:\n");
-    printf("1. View student data\n");
-    printf("2. Create new student data\n");
-    printf("3. Delete student data\n");
-    printf("4. Edit student data\n");
-    printf("5. Exit\n\n");
-
-    select();
-}
-
 int main()
 {
     menu();
